6b183e0b71cb406d5ee6b388909830da2f06df2a
Shahram <wf@belldex.com>
2020-03-13 15:35:48 -0700

    added few files

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,10 @@
 node default {
   file { '/root/README':
     ensure => file,
-    content => 'This is a readme, so read me',
+    content => 'This is a readme',
     owner   => 'root',
   }
+}
+node 'puppetmasterpo.lsst.org' {
+  include role::master_server
 }
