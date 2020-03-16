0e456dc311caaaa8c565e516ef2439e23155d84d
Shahram <wf@belldex.com>
2020-03-16 10:09:08 -0700

    double

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,7 +9,7 @@ node 'puppetmasterpo.lsst.org' {
   include role::master_server
   file {'/root/README':
     ensure => file,
-    content => “Welcome" to ${fqdn},
+    content => 'Welcome to ${fqdn}',
     owner => 'root',
 }
 node /^web/ {
