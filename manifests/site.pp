0278a5b62b3c046b26b99fbf85520d2f88cffcb4
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-05-13 15:11:50 -0700

    added yumrepo to site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,5 +17,14 @@ node /^web/ {
   include role::app_server
 }
 node 'webserver1.lsst.org' {
-  include role::db_server
+yumrepo { 'puppetrepo-products':
+  ensure    => 'present',
+  name      => 'puppetrepo-products',
+  descr     => 'Puppet Labs Products El 7 - $basearch',
+  baseurl   => 'http://myownmirror',
+  gpgkey    => 'http://myownmirror',
+  enabled   => '1',
+  gpgcheck  => '1',
+  target    => '/etc/yum.repo.d/puppetlabs.repo',
+}
 }
