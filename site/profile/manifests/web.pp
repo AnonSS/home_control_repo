c8c0b611dc0daca0d6cae160a6ce8ff385898090
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-05-15 10:00:13 -0700

    moved tcpdump to base

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -2,13 +2,8 @@ class profile::web {
   package {'httpd':
     ensure => present,
 }
-	package { 'tcpdump':
-		ensure => installed,
-	}
-  
   service { 'httpd':
     ensure => 'running',
     enable => 'true',
  }
 } 
-
