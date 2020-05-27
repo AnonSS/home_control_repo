5f249c122d45dda5cb4819a2c110edb107ae2541
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-05-27 10:17:32 -0700

    added yourls2

---
+++ site/profile/manifests/web2.pp
@@ -0,0 +1,8 @@
+class profile::web {
+  package {'httpd':
+    ensure => present,
+}
+  service { 'httpd':
+    ensure => 'running',
+    enable => 'true',
+ }
---
+++ site/role/manifests/yourls2_server.pp
@@ -0,0 +1,4 @@
+class role::yourls2_server {
+  include profile::base
+  include profile::web2
+}
