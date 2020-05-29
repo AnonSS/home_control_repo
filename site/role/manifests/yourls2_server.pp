f397bcc7b3f17c9cabfffbad90593152155b3e7c
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-05-29 14:37:35 -0700

    az

--- site/role/manifests/yourls2_server.pp
+++ site/role/manifests/yourls2_server.pp
@@ -1,4 +1,9 @@
 class role::yourls2_server {
   include profile::base
   include profile::web2
+    file {'/root/README':
+    ensure  => file,
+    content => 'Welcome to yourls2 server changed',
+    owner   => 'root',
+  }
 }
