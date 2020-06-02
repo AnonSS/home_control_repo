39b1bba0ebc3cbe9dbd74bdb21c472bbbb6d73ec
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-06-02 14:11:41 -0700

    corrected site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,31 +1,24 @@
----
-version: 5
-defaults:
-  data_hash: "yaml_data"
-hierarchy:
-  - name: "private hiera"
-    datadir: "/etc/puppetlabs/code/hieradata/private/%{environment}"
-    paths:
-      - "node/%{fqdn}.yaml"
-      - "site/%{site}/cluster/%{cluster}/role/%{role}.yaml"
-      - "site/%{site}/cluster/%{cluster}.yaml"
-      - "cluster/%{cluster}/role/%{role}.yaml"
-      - "cluster/%{cluster}.yaml"
-      - "site/%{site}/role/%{role}.yaml"
-      - "site/%{site}.yaml"
-      - "org/%{org}/role/%{role}.yaml"
-      - "role/%{role}.yaml"
-      - "org/%{org}.yaml"
-  - name: "public hiera"
-    datadir: "/etc/puppetlabs/code/environments/%{environment}/hieradata"
-    paths:
-      - "node/%{fqdn}.yaml"
-      - "site/%{site}/cluster/%{cluster}/role/%{role}.yaml"
-      - "site/%{site}/cluster/%{cluster}.yaml"
-      - "cluster/%{cluster}/role/%{role}.yaml"
-      - "cluster/%{cluster}.yaml"
-      - "site/%{site}/role/%{role}.yaml"
-      - "site/%{site}.yaml"
-      - "org/%{org}/role/%{role}.yaml"
-      - "role/%{role}.yaml"
-      - "org/%{org}.yaml"
+lookup('classes', Array[String], 'unique').include
+
+$files = lookup(
+  name          => 'files',
+  value_type    => Variant[Hash[String, Hash], Undef],
+  merge         => 'deep',
+  default_value => undef,
+)
+
+if ($files) {
+  ensure_resources('file', $files)
+}
+
+$packages = lookup(
+  name          => 'packages',
+  value_type    => Variant[Array[String], Undef],
+  merge         => 'unique',
+  default_value => undef,
+)
+
+if ($packages) {
+  ensure_packages($packages)
+}
+
