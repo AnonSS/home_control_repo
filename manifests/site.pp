ae6d0e603f5bcb988eefafeb4e7671916a842580
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-06-02 13:48:51 -0700

    changed site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,30 +1,31 @@
-node default {
-  file { '/root/README':
-    ensure  => file,
-    content => 'This is a readme3.',
-    owner   => 'root',
-  }
-}
-node 'puppetmasterpo' {
-  include role::master_server
-  file {'/root/README':
-    ensure  => file,
-    content => 'Welcome to puppetmasterpo today',
-    owner   => 'root',
-  }
-}
-#node /^web/ {
-#  include role::app_server
-#  include role::web_server
-#}
-node 'webserver1.lsst.org' {
-  include role::web_server
-  file {'/root/README':
-    ensure  => file,
-    content => 'Welcome to webserver1',
-    owner   => 'root',
-  }
-}
-node 'yourls2.lsst.org' {
-  include role::yourls2_server
-}
+---
+version: 5
+defaults:
+  data_hash: "yaml_data"
+hierarchy:
+  - name: "private hiera"
+    datadir: "/etc/puppetlabs/code/hieradata/private/%{environment}"
+    paths:
+      - "node/%{fqdn}.yaml"
+      - "site/%{site}/cluster/%{cluster}/role/%{role}.yaml"
+      - "site/%{site}/cluster/%{cluster}.yaml"
+      - "cluster/%{cluster}/role/%{role}.yaml"
+      - "cluster/%{cluster}.yaml"
+      - "site/%{site}/role/%{role}.yaml"
+      - "site/%{site}.yaml"
+      - "org/%{org}/role/%{role}.yaml"
+      - "role/%{role}.yaml"
+      - "org/%{org}.yaml"
+  - name: "public hiera"
+    datadir: "/etc/puppetlabs/code/environments/%{environment}/hieradata"
+    paths:
+      - "node/%{fqdn}.yaml"
+      - "site/%{site}/cluster/%{cluster}/role/%{role}.yaml"
+      - "site/%{site}/cluster/%{cluster}.yaml"
+      - "cluster/%{cluster}/role/%{role}.yaml"
+      - "cluster/%{cluster}.yaml"
+      - "site/%{site}/role/%{role}.yaml"
+      - "site/%{site}.yaml"
+      - "org/%{org}/role/%{role}.yaml"
+      - "role/%{role}.yaml"
+      - "org/%{org}.yaml"
