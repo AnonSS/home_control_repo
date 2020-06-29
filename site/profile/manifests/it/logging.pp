f47f113a56914b796493a770f13a46e773b32a52
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-06-28 19:00:13 -0700

    df

--- site/profile/manifests/it/logging.pp
+++ site/profile/manifests/it/logging.pp
@@ -4,13 +4,13 @@ class profile::it::logging {
 		package => 'java-1.8.0-openjdk',
 	}
 
-	class { 'mongodb::globals':
-		manage_package_repo => true,
-	}
+	# class { 'mongodb::globals':
+	# 	manage_package_repo => true,
+	# }
 
-	class { 'mongodb::server':
-		bind_ip => ['127.0.0.1'],
-	}
+	# class { 'mongodb::server':
+	# 	bind_ip => ['127.0.0.1'],
+	# }
 
 	$xms = lookup("elasticsearch_xms")
 	$xmx = lookup("elasticsearch_xmx")
