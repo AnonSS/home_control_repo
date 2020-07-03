4814bf0c229288255fd9d1321dc1c1e214e65405
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-07-03 07:46:23 -0700

    aa

--- site/profile/manifests/it/graylog.pp
+++ site/profile/manifests/it/graylog.pp
@@ -4,20 +4,20 @@ class profile::it::logging {
 		package => 'java-1.8.0-openjdk',
 	}
 
-	# class { 'mongodb::globals':
-	# 	manage_package_repo => true,
-	# }
+	class { 'mongodb::globals':
+		manage_package_repo => true,
+	}
 
-	# class { 'mongodb::server':
-	# 	bind_ip => ['127.0.0.1'],
-	# }
+	class { 'mongodb::server':
+		bind_ip => ['127.0.0.1'],
+	}
 
-	# $xms = lookup("elasticsearch_xms")
-	# $xmx = lookup("elasticsearch_xmx")
+	$xms = lookup("elasticsearch_xms")
+	$xmx = lookup("elasticsearch_xmx")
 
-	# class { 'elastic_stack::repo':
-	# 	version => 5,
-	# }
+	class { 'elastic_stack::repo':
+		version => 5,
+	}
 
 	class { 'elasticsearch':
 		#version      => '5.5.1',
@@ -38,7 +38,7 @@ class profile::it::logging {
 	class { 'graylog::repository':
 		version => '2.4'
 	}
-	
+
 	#################################################################################################################
 	# Requirements for graylog::server
 	$ssl_config_dir = "/etc/graylog/server"
@@ -47,11 +47,11 @@ class profile::it::logging {
 	file{ "/etc/graylog":
 		ensure => directory
 	}
-	
+
 	file{ $ssl_config_dir:
 		ensure => directory
 	}
-	
+
 	$ssl_config_filename = 'openssl-graylog.cnf'
 	$graylog_canonical_name = lookup("canonical_name")
 	file { "${ssl_config_dir}/${ssl_config_filename}":
@@ -59,9 +59,9 @@ class profile::it::logging {
 		mode    => '0644',
 		owner   => 'root',
 		group   => 'root',
-		content => epp('profile/it/graylog_ssl_cfg.epp', 
-			{ 
-				'country' => lookup("country"), 
+		content => epp('profile/it/graylog_ssl_cfg.epp',
+			{
+				'country' => lookup("country"),
 				'state' => lookup("state"),
 				'locality' => lookup("locality"),
 				'organization' => lookup("organization"),
@@ -73,31 +73,31 @@ class profile::it::logging {
 		),
 		require => [File[$ssl_config_dir], File_line["Update Graylog's JAVA_OPTS"]],
 	}
-	
+
 	$certificate_duration = lookup("certificate_duration")
 	$ssl_key_algorithm = lookup("ssl_key_algorithm")
 	$ssl_keyout_filename = "pkcs5-plain.pem"
 	$ssl_graylog_cert_filename = "graylog-certificate.pem"
 	exec{ "Create SSL certificate":
-		path  => [ '/usr/bin', '/bin', '/usr/sbin' ], 
+		path  => [ '/usr/bin', '/bin', '/usr/sbin' ],
 		command => "openssl req -x509 -days ${certificate_duration} -nodes -newkey ${ssl_key_algorithm} -config ${ssl_config_dir}/${ssl_config_filename} -keyout ${ssl_config_dir}/${ssl_keyout_filename} -out ${ssl_config_dir}/${ssl_graylog_cert_filename}",
 		onlyif => "test ! -f /etc/graylog/server/graylog-certificate.pem",
 		require => [File["${ssl_config_dir}/${ssl_config_filename}"]]
 	}
-	
+
 	# Convert PKCS5 into PKCS8 encrypted
-	
+
 	$ssl_pkcs8_encrypted_filename = "pkcs8-encrypted.pem"
 	$ssl_pkcs8_passout_phrase = lookup("ssl_passout_phrase")
-	
-	
+
+
 	exec{ "Convert pkcs5 to pkcs8 encrypted" :
 		path  => [ '/usr/bin', '/bin', '/usr/sbin' ],
 		command => "openssl pkcs8 -in ${ssl_config_dir}/${ssl_keyout_filename} -topk8 -out ${ssl_config_dir}/${ssl_pkcs8_encrypted_filename} -passout ${ssl_pkcs8_passout_phrase }",
 		onlyif => "test ! -f ${ssl_config_dir}/${ssl_pkcs8_encrypted_filename}",
 		require => Exec["Create SSL certificate"],
 	}
-	
+
 	# Create a SSL Key.
 
 	$ssl_key_passout_phrase = lookup("ssl_key_passout_phrase")
@@ -110,16 +110,16 @@ class profile::it::logging {
 	}
 
 	# Update the current keytool to include also this self signed certificate
-	
+
 	#Copy the current CA Cert into graylog's directory
-	
-	$graylog_cacert_filename = "graylog-cacerts"	
+
+	$graylog_cacert_filename = "graylog-cacerts"
 	exec{ "Copy JAVA cacerts into graylog's directory":
 		path  => [ '/usr/bin', '/bin', '/usr/sbin' ],
 		command => "cp /etc/pki/java/cacerts ${ssl_config_dir}/${graylog_cacert_filename}",
 		onlyif => "test ! -f ${ssl_config_dir}/${graylog_cacert_filename}"
 	}
-	
+
 	$ssl_graylog_cert_pass = lookup("ssl_graylog_cert_pass")
 	exec{ "Update keytool" :
 		path  => [ '/usr/bin', '/bin', '/usr/sbin' ],
@@ -131,7 +131,7 @@ class profile::it::logging {
 	# Setup Graylog server
 
 	$tls_password_array = split($ssl_key_passout_phrase, /:/)
-	$tls_cert_pass = $tls_password_array[1] 
+	$tls_cert_pass = $tls_password_array[1]
 	class { 'graylog::server':
 		package_version => '2.4.0-9',
 		config => {
