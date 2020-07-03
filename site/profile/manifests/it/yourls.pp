df336ae2726ef043708ee762fed5ba9a81ce41db
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-07-03 13:03:01 -0700

    aa

--- site/profile/manifests/it/yourls.pp
+++ site/profile/manifests/it/yourls.pp
@@ -20,7 +20,7 @@ class profile::it::yourls {
 		ensure => stopped,
 	}
 nginx::resource::server { 'yourls3.lsst.org':
-  listen_port => 81,
+  listen_port => 80,
   www_root    => '/var/www/yourls3.lsst.org',
 }
 }
