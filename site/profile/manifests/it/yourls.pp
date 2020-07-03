20fd4e8ef660b50394642236297ebb842e370aae
shahramsobhani <44172862+shahramsobhani@users.noreply.github.com>
2020-07-03 11:12:55 -0700

    aa

--- site/profile/manifests/it/yourls.pp
+++ site/profile/manifests/it/yourls.pp
@@ -1,5 +1,19 @@
 class profile::it::yourls {
   	package{"php-curl":
 		ensure => installed,
+	}  	package{"php":
+		ensure => installed,
+	}
+  	package{"php-mysql":
+		ensure => installed,
+	}
+  	package{"php-fpm":
+		ensure => installed,
+	}  
+  	package{"php-cli":
+		ensure => installed,
+	}
+  	package{"php-common":
+		ensure => installed,
 	}
 }
