85e0b98d36065be69419b1f177a7e0c9cb872028
AnonSS <mysticguy9@yahoo.com>
2020-07-15 10:46:35 -0700

    Update puppet_master.pp

--- site/profile/manifests/core/puppet_master.pp
+++ site/profile/manifests/core/puppet_master.pp
@@ -1,1 +1,6 @@
-class profile::core::puppet_master
+class profile::core::puppet_master {
+  file{ '/root/README':
+    ensure => file,
+    content => "Welcome to ${fqdn},\n BIOS release date:${bios_release_date} this is a Puppet Master Server\n",
+  }
+}
