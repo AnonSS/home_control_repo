69e47c97e4fc050a7083b87eb58447baba06a48b
AnonSS <mysticguy9@yahoo.com>
2020-07-17 08:08:56 -0700

    Update graylog.pp

--- site/profile/manifests/it/graylog.pp
+++ site/profile/manifests/it/graylog.pp
@@ -1,13 +1,13 @@
 class profile::it::graylog {
-	class { 'java' :
-		package => 'java-1.8.0-openjdk',
+  class { 'java' :
+     package => 'java-1.8.0-openjdk',
+ }
+
+  class { 'mongodb::globals':
+     manage_package_repo => true,
 	}
 
-	class { 'mongodb::globals':
-		manage_package_repo => true,
-	}
-
-	class { 'mongodb::server':
-		bind_ip => ['127.0.0.1'],
-	}
+  class { 'mongodb::server':
+     bind_ip => ['127.0.0.1'],
+ }
 }
