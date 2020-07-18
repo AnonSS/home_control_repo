62e27959b5d809e551521163618b9d2e10dfa866
AnonSS <mysticguy9@yahoo.com>
2020-07-18 14:17:01 -0700

    Update default.pp

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -1,11 +1,12 @@
 class profile::default {
 #  include profile::it::monitoring
 # All telegraf configuration came from Hiera
-
+include ssh
   Package { ensure => 'installed' }
 
   $enhancers = [ 'tree', 'sssd', 'realmd', 'oddjob', 'oddjob-mkhomedir', 'adcli',
-  'samba-common', 'samba-common-tools', 'krb5-workstation', 'openldap-clients', 'policycoreutils-python' ]
+  'samba-common', 'samba-common-tools', 'krb5-workstation', 'openldap-clients', 'policycoreutils-python',
+  'tcpdump', 'openssl', 'openssl-devel', 'telnet', 'acpid', 'lvm2', 'bash-completion', 'tree', 'sudo', ]
 
   package { $enhancers: }
   # Firewall and security measurements
