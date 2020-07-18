a6e1b2d34635d1d6fb41d2e2d4a127af0b19ab8d
AnonSS <mysticguy9@yahoo.com>
2020-07-18 14:09:16 -0700

    Update default.pp

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -1,9 +1,62 @@
 class profile::default {
-  include timezone
-  include ssh
-  include vim
 #  include profile::it::monitoring
 # All telegraf configuration came from Hiera
+	package { 'nmap':
+		ensure => installed,
+	}
+
+	package { 'vim':
+		ensure => installed,
+	}
+
+	package { 'wget':
+		ensure => installed,
+	}
+
+	# I would like to confirm if there is any particular version of gcc to be installed
+	package { 'gcc':
+		ensure => installed,
+	}
+
+	package { 'xinetd':
+		ensure => installed,
+	}
+
+	package { 'tcpdump':
+		ensure => installed,
+	}
+
+	package { 'openssl':
+		ensure => installed,
+	}
+
+	package { 'openssl-devel':
+		ensure => installed,
+	}
+
+	package { 'telnet':
+		ensure => installed,
+	}
+
+	package { 'acpid':
+		ensure => installed,
+	}
+
+	package { 'lvm2':
+		ensure => installed,
+	}
+	
+	package { 'bash-completion':
+		ensure => installed,
+	}
+
+	package { 'tree':
+		ensure => installed,
+	}
+
+	package { 'sudo':
+		ensure => installed,
+	}
   Package { ensure => 'installed' }
 
   $enhancers = [ 'tree', 'sssd', 'realmd', 'oddjob', 'oddjob-mkhomedir', 'adcli', 'samba-common', 'samba-common-tools', 'krb5-workstation', 'openldap-clients', 'policycoreutils-python' ]
