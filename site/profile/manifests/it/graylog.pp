69ff518ef22a1be3194914ef338a2d3df5349710
AnonSS <mysticguy9@yahoo.com>
2020-07-18 07:19:44 -0700

    Update graylog.pp

--- site/profile/manifests/it/graylog.pp
+++ site/profile/manifests/it/graylog.pp
@@ -1,34 +1,35 @@
 class profile::it::graylog {
-  class { 'java' :
-    package => 'java-1.8.0-openjdk',
- }
- class { 'mongodb::globals':
-  manage_package_repo => true,
-}->
-class { 'mongodb::server':
-  bind_ip => ['127.0.0.1'],
-}
-
-class { 'elasticsearch':
-  version      => '6.4.0',
-  repo_version => '6.4.0',
-  manage_repo  => true,
-}->
-elasticsearch::instance { 'graylog':
-  config => {
-    'cluster.name' => 'graylog',
-    'network.host' => '127.0.0.1',
-  }
-}
-
-class { 'graylog::repository':
+class { '::graylog::repository':
   version => '3.0'
 }->
-class { 'graylog::server':
-  package_version => '3.0.0-12',
-  config          => {
-    'password_secret' => 'test',    # Fill in your password secret
-#    'root_password_sha2' => '...', # Fill in your root password hash
-  }
+class { '::graylog::server':
+  config  => {
+    is_master                                          => true,
+    node_id_file                                       => '/etc/graylog/server/node-id',
+    password_secret                                    => 'password_secret',
+    root_username                                      => 'admin',
+    root_password_sha2                                 => 'root_password_sha2',
+    root_timezone                                      => 'Europe/Berlin',
+    allow_leading_wildcard_searches                    => true,
+    allow_highlighting                                 => true,
+    http_bind_address                                  => '0.0.0.0:9000',
+    http_external_uri                                  => 'https://graylog.home.vm:9000/',
+    http_enable_tls                                    => true,
+    http_tls_cert_file                                 => '/etc/ssl/graylog/graylog_cert_chain.crt',
+    http_tls_key_file                                  => '/etc/ssl/graylog/graylog_key_pkcs8.pem',
+    http_tls_key_password                              => 'sslkey-password',
+    rotation_strategy                                  => 'time',
+    retention_strategy                                 => 'delete',
+    elasticsearch_max_time_per_index                   => '1d',
+    elasticsearch_max_number_of_indices                => '30',
+    elasticsearch_shards                               => '4',
+    elasticsearch_replicas                             => '1',
+    elasticsearch_index_prefix                         => 'graylog',
+    elasticsearch_hosts                                => 'http://elasticsearch01.domain.local:9200,http://elasticsearch02.domain.local:9200',
+    mongodb_uri                                        => 'mongodb://mongouser:mongopass@mongodb01.domain.local:27017,mongodb02.domain.local:27017,mongodb03.domain.local:27017/graylog',
+  },
+  require => Class[
+    '::java',
+  ],
 }
 }
