6cf93b60c13af0c27b5b4dd8a7b1d49384ca2848
AnonSS <mysticguy9@yahoo.com>
2020-07-19 16:32:34 -0700

    Update graylog3.pp

--- site/profile/manifests/it/graylog3.pp
+++ site/profile/manifests/it/graylog3.pp
@@ -2,18 +2,4 @@ class profile::it::graylog3 {
 	class { 'java' :
 		package => 'java-1.8.0-openjdk',
 	}
-
-class {'mongodb::globals':
-  manage_package_repo => true,
-  version             => '3.6',
-}
-
-class {'mongodb::server':
-  auth => true,
-}
-
-mongodb::db { 'testdb':
-  user          => 'user1',
-  password_hash => 'a15fbfca5e3a758be80ceaf42458bcd8',
-}
 }  
