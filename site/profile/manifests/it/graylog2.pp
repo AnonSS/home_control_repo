a308a04a576e15e7ca5615cc325aa36437c72049
AnonSS <mysticguy9@yahoo.com>
2020-07-19 13:28:53 -0700

    Update graylog2.pp

--- site/profile/manifests/it/graylog2.pp
+++ site/profile/manifests/it/graylog2.pp
@@ -91,6 +91,36 @@ class profile::it::graylog2 {
 		require => [Exec["Create graylog SSL key"], Exec["Copy JAVA cacerts into graylog's directory"]]
 	}
 
+	class { 'mongodb::globals':
+		manage_package_repo => true,
+	}
+
+	class { 'mongodb::server':
+		bind_ip => ['127.0.0.1'],
+	}
+
+	$xms = lookup("elasticsearch_xms")
+	$xmx = lookup("elasticsearch_xmx")
+
+	class { 'elastic_stack::repo':
+		version => 6,
+	}
+
+	class { 'elasticsearch':
+		version      => '6.3.1',
+		manage_repo  => true,
+		jvm_options => [
+			"-Xms${xms}",
+			"-Xmx${xmx}"
+		]
+	}
+
+	elasticsearch::instance { 'graylog':
+		config => {
+			'cluster.name' => 'graylog',
+			'network.host' => '127.0.0.1',
+		}
+	}
 
 
 class { '::graylog::repository':
