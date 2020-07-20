a6ced740f37929c5e5d54e37aed920355a6ef970
AnonSS <mysticguy9@yahoo.com>
2020-07-19 18:12:00 -0700

    Update graylog3.pp

--- site/profile/manifests/it/graylog3.pp
+++ site/profile/manifests/it/graylog3.pp
@@ -121,4 +121,13 @@ class { 'mongodb::server':
 		onlyif => "test -z $(keytool -keystore ${ssl_config_dir}/${graylog_cacert_filename} -storepass ${ssl_graylog_cert_pass} -list | grep graylog-self-signed)",
 		require => [Exec["Create graylog SSL key"], Exec["Copy JAVA cacerts into graylog's directory"]]
 	}
+	$graylog_java_opts = lookup("graylog_java_opts")
+	#Update graylog JAVA_OPTS to use the customized version including the self-signed cert.
+	file_line{ "Update Graylog's JAVA_OPTS":
+		ensure => present,
+		path => "/etc/sysconfig/graylog-server",
+		line => "GRAYLOG_SERVER_JAVA_OPTS=\"${graylog_java_opts} -Djavax.net.ssl.trustStore=${ssl_config_dir}/${graylog_cacert_filename}\"",
+		match => "GRAYLOG_SERVER_JAVA_OPTS*",
+		require => Class["graylog::server"]
+	}
 }  
