4af5f09bd5415bc2b05e8ae7c839f9803b51f905
AnonSS <mysticguy9@yahoo.com>
2020-07-20 10:35:19 -0700

    Update default.pp

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -2,6 +2,7 @@ class profile::default {
 #  include profile::it::monitoring
 # All telegraf configuration came from Hiera
 include ssh
+include augeas
   Package { ensure => 'installed' }
 
   $enhancers = [ 'tree', 'sssd', 'realmd', 'oddjob', 'oddjob-mkhomedir', 'adcli',
