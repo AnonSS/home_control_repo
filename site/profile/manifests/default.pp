b8db9350bbaebb550fa369bd098f0d01b3f16fb5
AnonSS <mysticguy9@yahoo.com>
2020-07-21 13:04:20 -0700

    Update default.pp

--- site/profile/manifests/default.pp
+++ site/profile/manifests/default.pp
@@ -16,4 +16,33 @@ include augeas
 		line => 'SELINUX=enforce',
 		match => '^SELINUX=+',
 	}  
+	$lsst_firewall_default_zone = lookup("lsst_firewall_default_zone")
+
+	class { "firewalld":
+		service_ensure => lookup("firewalld_status"),
+		default_zone => $lsst_firewall_default_zone,
+	}
+
+	firewalld_zone { $lsst_firewall_default_zone:
+		ensure => present,
+		target => lookup("lsst_firewall_default_target"),
+		sources => lookup("lsst_firewall_default_sources")
+	}
+
+	firewalld_service { 'Enable SSH':
+		ensure  => 'present',
+		service => 'ssh',
+	}
+
+	firewalld_service { 'Enable DHCP':
+		ensure  => 'present',
+		service => 'dhcpv6-client',
+	}
+
+	exec{"enable_icmp":
+		provider => "shell",
+		command => "/usr/bin/firewall-cmd --add-protocol=icmp --permanent && /usr/bin/firewall-cmd --reload",
+		require => Class["firewalld"],
+		onlyif => "[[ \"\$(firewall-cmd --list-protocols)\" != *\"icmp\"* ]]"
+	}
 }
