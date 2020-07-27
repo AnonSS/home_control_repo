c959aaa84cc5627c49e9d3712942b4ce6f2be9ba
AnonSS <mysticguy9@yahoo.com>
2020-07-27 13:12:10 -0700

    Create common.pp

---
+++ site/profile/manifests/core/common.pp
@@ -0,0 +1,34 @@
+# @summary
+#   Common functionality needed by standard nodes.
+#
+# @param collect_metrics
+#   Enable or disable metrics collection. Metrics collection may be disabled on development
+#   nodes, nodes that don't have uptime requirements, or nodes that should only have minimal
+#   software load.
+class profile::core::common(
+  Boolean $collect_metrics = true,
+) {
+  include timezone
+  include tuned
+  include chrony
+  include selinux
+  include firewall
+  include irqbalance
+  include sysstat
+  include epel
+  include sudo
+  include accounts
+  include puppet_agent
+  include resolv_conf
+  include ssh
+  include easy_ipa
+  include augeas
+  include rsyslog
+  include rsyslog::config
+  include profile::core::hardware
+  include profile::core::dielibwrapdie
+
+  if $collect_metrics {
+    include profile::core::telegraf
+  }
+}
